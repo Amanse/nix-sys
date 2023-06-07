0f60058ba86cc86d9fad6e613c788e9480c9f6f8
AmanSe <amansetiarjp@gmail.com>
2023-06-07 23:16:38 +0530

    tmux: fix spliting pane command

--- nixpkgs/home/terminal/tmux.nix
+++ nixpkgs/home/terminal/tmux.nix
@@ -1,5 +1,4 @@
-{ pkgs, ... }:
-{
+{pkgs, ...}: {
   programs.tmux = {
     enable = true;
     shell = "${pkgs.zsh}/bin/zsh";
@@ -14,7 +13,7 @@
       tmuxPlugins.better-mouse-mode
     ];
 
-     extraConfig = ''
+    extraConfig = ''
       # https://old.reddit.com/r/tmux/comments/mesrci/tmux_2_doesnt_seem_to_use_256_colors/
       set -g default-terminal "xterm-256color"
       set -ga terminal-overrides ",*256col*:Tc"
@@ -24,7 +23,8 @@
       # Mouse works as expected
       set-option -g mouse on
       # easy-to-remember split pane commands
-      bind \ split-window -h -c "#{pane_current_path}"
+      # https://github.com/tmux/tmux/issues/1990#issuecomment-560477212
+      bind \\ split-window -h -c "#{pane_current_path}"
       bind - split-window -v -c "#{pane_current_path}"
       bind c new-window -c "#{pane_current_path}"
     '';
