5b85f07f2a67cce40dbdb1fe2a44821f74d987c3
AmanSe <amansetiarjp@gmail.com>
2024-02-14 18:29:15 +0530

    impermanence: cleanup and optional

--- modules/nixos/system/impermanence/default.nix
+++ modules/nixos/system/impermanence/default.nix
@@ -44,19 +44,21 @@ in {
           "/etc/nixos"
           "/etc/nix"
           "/etc/NetworkManager/system-connections"
-          "/etc/secureboot"
           "/var/db/sudo"
-          "/var/lib/flatpak"
-          "/var/lib/libvirt"
           "/var/lib/bluetooth"
           "/var/lib/nixos"
           "/var/lib/pipewire"
           "/var/lib/systemd/coredump"
           "/var/lib/systemd/backlight"
+        ]
+        ++ [config.programs.ccache.cacheDir]
+        ++ lib.optionals config.services.tailscale.enable [
           "/var/cache/tailscale"
           "/var/lib/tailscale"
         ]
-        ++ [config.programs.ccache.cacheDir];
+        ++ lib.optional config.myModules.boot.secure-boot.enable "/etc/secureboot"
+        ++ lib.optional config.services.flatpak.enable "/var/lib/flatpak"
+        ++ lib.optional config.virtualisation.libvirtd.enable "/var/lib/libvirt";
 
       files = [
         # important state
@@ -128,49 +130,5 @@ in {
       # we can unmount /mnt and continue on the boot process.
       umount /mnt
     '';
-
-    # boot.initrd.systemd.services.rollback = {
-    #   description = "Rollback BTRFS root subvolume to a pristine state";
-    #   wantedBy = ["initrd.target"];
-    #   # make sure it's done after encryption
-    #   # i.e. LUKS/TPM process
-    #   # mount the root fs before clearing
-    #   before = ["sysroot.mount"];
-    #   unitConfig.DefaultDependencies = "no";
-    #   serviceConfig.Type = "oneshot";
-    #   script = ''
-    #     mkdir -p /mnt
-    #
-    #     # We first mount the btrfs root to /mnt
-    #     # so we can manipulate btrfs subvolumes.
-    #     mount -o subvol=/ /dev/disk/by-label/NIXOS /mnt
-    #
-    #     # While we're tempted to just delete /root and create
-    #     # a new snapshot from /root-blank, /root is already
-    #     # populated at this point with a number of subvolumes,
-    #     # which makes `btrfs subvolume delete` fail.
-    #     # So, we remove them first.
-    #     #
-    #     # /root contains subvolumes:
-    #     # - /root/var/lib/portables
-    #     # - /root/var/lib/machines
-    #
-    #     btrfs subvolume list -o /mnt/root |
-    #       cut -f9 -d' ' |
-    #       while read subvolume; do
-    #         echo "deleting /$subvolume subvolume..."
-    #         btrfs subvolume delete "/mnt/$subvolume"
-    #       done &&
-    #       echo "deleting /root subvolume..." &&
-    #       btrfs subvolume delete /mnt/root
-    #
-    #     echo "restoring blank /root subvolume..."
-    #     btrfs subvolume snapshot /mnt/root-blank /mnt/root
-    #
-    #     # Once we're done rolling back to a blank snapshot,
-    #     # we can unmount /mnt and continue on the boot process.
-    #     umount /mnt
-    #   '';
-    # };
   };
 }
