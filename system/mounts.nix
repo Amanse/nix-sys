1c0c8aa5f0389915ae2ca15e3584792b28dd9938
AmanSe <amansetiarjp@gmail.com>
2024-02-18 17:03:01 +0530

    a time

--- system/mounts.nix
+++ system/mounts.nix
@@ -1,10 +1,12 @@
 {...}: {
   fileSystems."/mnt/stuff" = {
     device = "/dev/disk/by-label/stuff";
+    options = ["noatime"];
   };
 
   fileSystems."/mnt/stuff2" = {
     device = "/dev/disk/by-label/stuff2";
+    options = ["noatime"];
   };
 
   boot.tmp.useTmpfs = true;
