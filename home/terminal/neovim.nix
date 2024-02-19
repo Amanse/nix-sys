73ba867d3e7a08f74496b15b4053e392b0f2a2e0
AmanSe <amansetiarjp@gmail.com>
2024-02-19 16:49:50 +0530

    Bufferline and got lsp to refactor finally

--- home/terminal/neovim.nix
+++ home/terminal/neovim.nix
@@ -4,144 +4,153 @@
   programs.neovim-flake = {
     enable = true;
     settings = {
-      vim.lsp = {
-        enable = true;
-        formatOnSave = true;
-        nvimCodeActionMenu.enable = true;
-        lightbulb.enable = true;
-      };
-
-      vim.useSystemClipboard = false;
-
-      vim.languages = {
-        enableLSP = true;
-        enableFormat = true;
-        enableTreesitter = true;
-
-        nix.enable = true;
-        html.enable = true;
-        clang.enable = true;
-        rust = {
+      vim = {
+        lsp = {
           enable = true;
-          crates.enable = true;
+          formatOnSave = true;
+          nvimCodeActionMenu.enable = true;
+          lightbulb.enable = true;
         };
-        java.enable = true;
-        java.lsp.enable = true;
-        java.treesitter.enable = true;
-        markdown.enable = true;
-        # markdown.glow.enable = true;
-        ts = {
-          enable = true;
-          lsp.enable = true;
-          treesitter = {
+
+        useSystemClipboard = false;
+
+        languages = {
+          enableLSP = true;
+          enableFormat = true;
+          enableTreesitter = true;
+
+          nix.enable = true;
+          html.enable = true;
+          clang.enable = true;
+          rust = {
             enable = true;
+            crates.enable = true;
+          };
+          java.enable = true;
+          java.lsp.enable = true;
+          java.treesitter.enable = true;
+          markdown.enable = true;
+          # markdown.glow.enable = true;
+          ts = {
+            enable = true;
+            lsp.enable = true;
+            treesitter = {
+              enable = true;
+            };
+          };
+          go.enable = true;
+          svelte.enable = true;
+        };
+
+        treesitter = {
+          enable = true;
+          grammars = [pkgs.tree-sitter-grammars.tree-sitter-svelte pkgs.tree-sitter-grammars.tree-sitter-typescript pkgs.tree-sitter-grammars.tree-sitter-embedded-template];
+        };
+
+        extraPlugins = with pkgs.vimPlugins; {
+          undodir = {
+            package = undotree;
+            setup = ''
+              vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+              vim.opt.undofile = true
+            '';
           };
         };
-        go.enable = true;
-        svelte.enable = true;
-      };
 
-      vim.treesitter = {
-        enable = true;
-        grammars = [pkgs.tree-sitter-grammars.tree-sitter-svelte pkgs.tree-sitter-grammars.tree-sitter-typescript pkgs.tree-sitter-grammars.tree-sitter-embedded-template];
-      };
-
-      vim.extraPlugins = with pkgs.vimPlugins; {
-        undodir = {
-          package = undotree;
-          setup = ''
-            vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
-            vim.opt.undofile = true
-          '';
+        # vim.startPlugins = [pkgs.vimPlugins.undotree pkgs.vimPlugins.harpoon];
+        luaConfigRC = {
+          after = "vim.filetype.add({extension = {ejs = \"html\"}})\n";
         };
-      };
 
-      # vim.startPlugins = [pkgs.vimPlugins.undotree pkgs.vimPlugins.harpoon];
-      vim.luaConfigRC = {
-        after = "vim.filetype.add({extension = {ejs = \"html\"}})\n";
-      };
+        preventJunkFiles = true;
 
-      vim.preventJunkFiles = true;
-
-      vim.visuals = {
-        enable = true;
-        nvimWebDevicons.enable = true;
-        indentBlankline = {
+        visuals = {
           enable = true;
-          fillChar = null;
-          eolChar = null;
-          scope.enabled = true;
+          nvimWebDevicons.enable = true;
+          indentBlankline = {
+            enable = true;
+            fillChar = null;
+            eolChar = null;
+            scope.enabled = true;
+          };
+          highlight-undo.enable = true;
+          # cursorline.enable = true;
+          fidget-nvim.enable = true;
+          # cursorWordline = {
+          #   enable = true;
+          #   lineTimeout = 0;
+          # };
         };
-        highlight-undo.enable = true;
-        # cursorline.enable = true;
-        fidget-nvim.enable = true;
-        # cursorWordline = {
-        #   enable = true;
-        #   lineTimeout = 0;
-        # };
-      };
 
-      vim.statusline = {
-        lualine = {
+        statusline = {
+          lualine = {
+            enable = true;
+            theme = "catppuccin";
+          };
+        };
+
+        binds = {
+          whichKey.enable = true;
+        };
+
+        git = {
           enable = true;
-          theme = "catppuccin";
+          gitsigns.enable = true;
+          # gitsigns.codeActions = true;
         };
-      };
 
-      vim.binds = {
-        whichKey.enable = true;
-      };
-
-      vim.git = {
-        enable = true;
-        gitsigns.enable = true;
-        # gitsigns.codeActions = true;
-      };
-
-      vim.comments = {
-        comment-nvim.enable = true;
-      };
-
-      vim.theme = {
-        enable = true;
-        name = "catppuccin";
-        style = "mocha";
-        transparent = true;
-      };
-      vim.autopairs.enable = true;
-
-      vim.autocomplete = {
-        enable = true;
-        type = "nvim-cmp";
-      };
-
-      vim.telescope = {
-        enable = true;
-        mappings = {
-          findFiles = "<leader>pf";
+        comments = {
+          comment-nvim.enable = true;
         };
-      };
 
-      vim.ui = {
-        breadcrumbs.enable = true;
-      };
-
-      vim.maps = {
-        normal = {
-          # "<leader>pf" = {action = ":Telescope find_files<CR>";};
-          # "<leader>p" = {action = "\"_dP";};
-          "<leader>y" = {action = "\"+y";};
-          "<leader>u" = {action = ":UndotreeToggle<CR>";};
-          "<leader>f" = {action = ":lua vim.lsp.buf.format()<CR>";};
-          "<leader>d" = {action = "\"_d";};
-          # "<leader>a" = ":lua require(\"harpoon.mark\").add_file()<CR>";
-          # "<leader>q" = ":lua require(\"harpoon.ui\").toggle_quick_menu()<CR>";
-          "<leader>pv" = {action = ":E<CR>";};
+        theme = {
+          enable = true;
+          name = "catppuccin";
+          style = "mocha";
+          transparent = true;
         };
-        visual = {
-          "<leader>y" = {action = "\"+y";};
-          "<leader>p" = {action = "\"_dP";};
-          "<leader>d" = {action = "\"_d";};
+        autopairs.enable = true;
+
+        autocomplete = {
+          enable = true;
+          type = "nvim-cmp";
+        };
+
+        telescope = {
+          enable = true;
+          mappings = {
+            findFiles = "<leader>pf";
+          };
+        };
+
+        tabline.nvimBufferline = {
+          enable = true;
+          mappings = {
+            closeCurrent = "<leader>bx";
+          };
+        };
+
+        ui = {
+          breadcrumbs.enable = true;
+        };
+
+        maps = {
+          normal = {
+            # "<leader>pf" = {action = ":Telescope find_files<CR>";};
+            # "<leader>p" = {action = "\"_dP";};
+            "<leader>y" = {action = "\"+y";};
+            "<leader>u" = {action = ":UndotreeToggle<CR>";};
+            "<leader>f" = {action = ":lua vim.lsp.buf.format()<CR>";};
+            "<leader>d" = {action = "\"_d";};
+            # "<leader>a" = ":lua require(\"harpoon.mark\").add_file()<CR>";
+            # "<leader>q" = ":lua require(\"harpoon.ui\").toggle_quick_menu()<CR>";
+            "<leader>pv" = {action = ":E<CR>";};
+          };
+          visual = {
+            "<leader>y" = {action = "\"+y";};
+            "<leader>p" = {action = "\"_dP";};
+            "<leader>d" = {action = "\"_d";};
+          };
         };
       };
     };
