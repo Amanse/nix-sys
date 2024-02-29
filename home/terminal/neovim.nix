13e72642ae5487791a3c783da0de8ec76a6583e1
AmanSe <amansetiarjp@gmail.com>
2024-02-29 20:01:20 +0530

    neovim: enable tailwind lsp

--- home/terminal/neovim.nix
+++ home/terminal/neovim.nix
@@ -30,6 +30,7 @@
           java.lsp.enable = true;
           java.treesitter.enable = true;
           markdown.enable = true;
+          tailwind.enable = true;
           # markdown.glow.enable = true;
           ts = {
             enable = true;
@@ -51,38 +52,38 @@
           harpoon = {
             package = pkgs.harpoon;
             setup = ''
-                            local harpoon = require("harpoon")
+                                          local harpoon = require("harpoon")
 
-                            -- REQUIRED
-                            harpoon:setup()
-              harpoon:setup({})
-                            -- REQUIRED
+                                          -- REQUIRED
+                                          harpoon:setup()
+                            harpoon:setup({})
+                                          -- REQUIRED
 
-vim.keymap.set("n", "<C-a>", function() harpoon:list():select(1) end)
-vim.keymap.set("n", "<C-s>", function() harpoon:list():select(2) end)
-vim.keymap.set("n", "<C-d>", function() harpoon:list():select(3) end)
-vim.keymap.set("n", "<C-f>", function() harpoon:list():select(4) end)
+              vim.keymap.set("n", "<C-a>", function() harpoon:list():select(1) end)
+              vim.keymap.set("n", "<C-s>", function() harpoon:list():select(2) end)
+              vim.keymap.set("n", "<C-d>", function() harpoon:list():select(3) end)
+              vim.keymap.set("n", "<C-f>", function() harpoon:list():select(4) end)
 
-              -- basic telescope configuration
-              local conf = require("telescope.config").values
-              local function toggle_telescope(harpoon_files)
-                  local file_paths = {}
-                  for _, item in ipairs(harpoon_files.items) do
-                      table.insert(file_paths, item.value)
-                  end
+                            -- basic telescope configuration
+                            local conf = require("telescope.config").values
+                            local function toggle_telescope(harpoon_files)
+                                local file_paths = {}
+                                for _, item in ipairs(harpoon_files.items) do
+                                    table.insert(file_paths, item.value)
+                                end
 
-                  require("telescope.pickers").new({}, {
-                      prompt_title = "Harpoon",
-                      finder = require("telescope.finders").new_table({
-                          results = file_paths,
-                      }),
-                      previewer = conf.file_previewer({}),
-                      sorter = conf.generic_sorter({}),
-                  }):find()
-              end
+                                require("telescope.pickers").new({}, {
+                                    prompt_title = "Harpoon",
+                                    finder = require("telescope.finders").new_table({
+                                        results = file_paths,
+                                    }),
+                                    previewer = conf.file_previewer({}),
+                                    sorter = conf.generic_sorter({}),
+                                }):find()
+                            end
 
-              vim.keymap.set("n", "<C-e>", function() toggle_telescope(harpoon:list()) end,
-    { desc = "Open harpoon window" })
+                            vim.keymap.set("n", "<C-e>", function() toggle_telescope(harpoon:list()) end,
+                  { desc = "Open harpoon window" })
             '';
             after = ["plenary-nvim"];
           };
